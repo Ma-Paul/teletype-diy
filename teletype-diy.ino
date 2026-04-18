ca5ad9d530bd5deed2581be609bafcbf0f4138d8
ma-paul <no_need_to_know@youre_a_stalker.goaway>
2026-04-18 11:48:18 +0200

    Merged Conflict Solved

--- teletype-diy.ino
+++ teletype-diy.ino
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 void setup()
 {
     Serial.begin(19200);
@@ -8,13 +7,5 @@ void
 loop()
 {
     Serial.print("Paul rulez\n");
-=======
-void setup() {
-    Serial.begin(19200);
-}
-
-void loop() {
-    Serial.println("Hello, world!");
->>>>>>> 1b784750be88d8c4f2bcc8c6abbb5b8bee0ea392
     delay(1000);
 }
