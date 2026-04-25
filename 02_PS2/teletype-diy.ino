c14b20ce82c848ab363684cae334f5255274b38d
jj-cremer <julius.c.0309@gmail.com>
2026-04-25 11:18:07 +0200

    Started to translate scan to ASCII

--- 02_PS2/teletype-diy.ino
+++ 02_PS2/teletype-diy.ino
@@ -1,4 +1,5 @@
-void setup()
+void 
+setup()
 {
     Serial.begin(19200);
 }
@@ -7,8 +8,13 @@ void
 loop()
 {
     if (Serial.available() > 0) {
-        unsigned x = Serial.read();
-        Serial.println(x, HEX);
+        char x = Serial.read();
+        switch (x) {
+            case 0x1C: x = 'A'; break;
+            case 0x42: x = 'K'; break;
+            default: x = '?'; break;
+        }
+        Serial.println(x);
     }
 
 }
