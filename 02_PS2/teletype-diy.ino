618115f89f18237af803885859a8b8b7a8b84e1a
ma-paul <no_need_to_know@youre_a_stalker.goaway>
2026-04-25 11:40:35 +0200

    Letters

--- 02_PS2/teletype-diy.ino
+++ 02_PS2/teletype-diy.ino
@@ -19,7 +19,26 @@ loop()
         switch (x) {
             case 0x1C: x = 'A'; break;
             case 0x42: x = 'K'; break;
-            default: x = '?'; break;
+            case 0x16: x = '1'; break;
+            case 0x1E: x = '2'; break;
+            case 0x26: x = '3'; break;
+            case 0x25: x = '4'; break;
+            case 0x2E: x = '5'; break;
+            case 0x36: x = '6'; break;
+            case 0x3D: x = '7'; break;
+            case 0x3E: x = '8'; break;
+            case 0x46: x = '9'; break;
+            case 0x45: x = '0'; break;
+            case 0x15: x = 'Q'; break;
+            case 0x1D: x = 'W'; break;
+            case 0x24: x = 'E'; break;
+            case 0x2D: x = 'R'; break;
+            case 0x2C: x = 'T'; break;
+            case 0x35: x = 'Y'; break;
+            case 0x3C: x = 'U'; break;
+            case 0x43: x = 'I'; break;
+            case 0x44: x = 'O'; break;
+            case 0x4D: x = 'P'; break;
         }
         Serial.println(x);
     }
