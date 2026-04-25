b884f60c3812adee71be2d81d6d9905ea6e05c82
jj-cremer <julius.c.0309@gmail.com>
2026-04-25 11:41:47 +0200

    Letters A to M

--- 02_PS2/teletype-diy.ino
+++ 02_PS2/teletype-diy.ino
@@ -19,6 +19,22 @@ loop()
         switch (x) {
             case 0x1C: x = 'A'; break;
             case 0x42: x = 'K'; break;
+            case 0x1B: x = 'S'; break;
+            case 0x23: x = 'D'; break;
+            case 0x2B: x = 'F'; break;
+            case 0x34: x = 'G'; break;
+            case 0x33: x = 'H'; break;
+            case 0x3B: x = 'J'; break;
+            case 0x05: x = 'L'; break;
+            case 0x0C: x = '\n'; break;
+            case 0x0B: x = 'Z'; break;
+            case 0x83: x = 'X'; break;
+            case 0x0A: x = 'C'; break;
+            case 0x01: x = 'V'; break;
+            case 0x09: x = 'B'; break;
+            case 0x78: x = 'N'; break;
+            case 0x07: x = 'M'; break;
+            
             default: x = '?'; break;
         }
         Serial.println(x);
