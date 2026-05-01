fd62eb41f3ccc3388b0414729b4601d4d783db36
jj-cremer <julius.c.0309@gmail.com>
2026-05-01 11:56:48 +0200

    Here u go Paulo

--- 02_PS2/teletype-diy.ino
+++ 02_PS2/teletype-diy.ino
@@ -3,7 +3,7 @@
  * RX Arduino <-> TX RS234
  * During upload: disconnect RX!
 */ 
-
+bool uppercase;
 int indent;
 
 void setup()
@@ -22,45 +22,46 @@ loop()
                 Serial.println(x, HEX);
                 x = 0; 
                 break;
-            case 0x1C: x = 'A'; break;
-            case 0x42: x = 'K'; break;
-            case 0x1B: x = 'S'; break;
-            case 0x23: x = 'D'; break;
-            case 0x2B: x = 'F'; break;
-            case 0x34: x = 'G'; break;
-            case 0x33: x = 'H'; break;
-            case 0x3B: x = 'J'; break;
-            case 0x4B: x = 'L'; break;
+            case 0x58: x = 0; uppercase = !uppercase; break;
             case 0x5A: x = '\n'; break;
-            case 0x1A: x = 'Z'; break;
-            case 0x22: x = 'X'; break;
-            case 0x21: x = 'C'; break;
-            case 0x2A: x = 'V'; break;
-            case 0x32: x = 'B'; break;
-            case 0x31: x = 'N'; break;
-            case 0x3A: x = 'M'; break;
-            case 0x16: x = '1'; break;
-            case 0x1E: x = '2'; break;
-            case 0x26: x = '3'; break;
-            case 0x25: x = '4'; break;
-            case 0x2E: x = '5'; break;
-            case 0x36: x = '6'; break;
-            case 0x3D: x = '7'; break;
-            case 0x3E: x = '8'; break;
-            case 0x46: x = '9'; break;
-            case 0x45: x = '0'; break;
-            case 0x15: x = 'Q'; break;
-            case 0x1D: x = 'W'; break;
-            case 0x24: x = 'E'; break;
-            case 0x2D: x = 'R'; break;
-            case 0x2C: x = 'T'; break;
-            case 0x35: x = 'Y'; break;
-            case 0x3C: x = 'U'; break;
-            case 0x43: x = 'I'; break;
-            case 0x44: x = 'O'; break;
-            case 0x4D: x = 'P'; break;
+            case 0x1C: x = uppercase ? 'A' : 'a'; break;
+            case 0x42: x = uppercase ? 'K' : 'k'; break;
+            case 0x1B: x = uppercase ? 'S' : 's'; break;
+            case 0x23: x = uppercase ? 'D' : 'd'; break;
+            case 0x2B: x = uppercase ? 'F' : 'f'; break;
+            case 0x34: x = uppercase ? 'G' : 'g'; break;
+            case 0x33: x = uppercase ? 'H' : 'h'; break;
+            case 0x3B: x = uppercase ? 'J' : 'j'; break;
+            case 0x4B: x = uppercase ? 'L' : 'l'; break;
+            case 0x1A: x = uppercase ? 'Z' : 'z'; break;
+            case 0x22: x = uppercase ? 'X' : 'x'; break;
+            case 0x21: x = uppercase ? 'C' : 'c'; break;
+            case 0x2A: x = uppercase ? 'V' : 'v'; break;
+            case 0x32: x = uppercase ? 'B' : 'b'; break;
+            case 0x31: x = uppercase ? 'N' : 'n'; break;
+            case 0x3A: x = uppercase ? 'M' : 'm'; break;
+            case 0x16: x = uppercase ? '!' : '1'; break;
+            case 0x1E: x = uppercase ? '@' : '2'; break;
+            case 0x26: x = uppercase ? '#' : '3'; break;
+            case 0x25: x = uppercase ? '$' : '4'; break;
+            case 0x2E: x = uppercase ? '%' : '5'; break;
+            case 0x36: x = uppercase ? '^' : '6'; break;
+            case 0x3D: x = uppercase ? '&' : '7'; break;
+            case 0x3E: x = uppercase ? '*' : '8'; break;
+            case 0x46: x = uppercase ? '(' : '9'; break;
+            case 0x45: x = uppercase ? ')' : '0'; break;
+            case 0x15: x = uppercase ? 'Q' : 'q'; break;
+            case 0x1D: x = uppercase ? 'W' : 'w'; break;
+            case 0x24: x = uppercase ? 'E' : 'e'; break;
+            case 0x2D: x = uppercase ? 'R' : 'r'; break;
+            case 0x2C: x = uppercase ? 'T' : 't'; break;
+            case 0x35: x = uppercase ? 'Y' : 'y'; break;
+            case 0x3C: x = uppercase ? 'U' : 'u'; break;
+            case 0x43: x = uppercase ? 'I' : 'i'; break;
+            case 0x44: x = uppercase ? 'O' : 'o'; break;
+            case 0x4D: x = uppercase ? 'P' : 'p'; break;
             case 0x29: x = ' '; break;
-
+    
         }
         if (x == '\n') {
             indent = 0;
